982a4e2eb3c4b6180c6e5248cda53a30cb323a3d
JackTrusler <jtrus93@gmail.com>
2022-09-25 20:16:06 -0400

    anchor

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import cutLogo from '../../public/theCutLogo.png'
-import {Barber} from './Barbers/Barber';
-import {Dispatch, SetStateAction, useState} from 'react';
-import {BarberData} from '../data/barberData';
+import { Barber } from './Barbers/Barber';
+import { Dispatch, SetStateAction, useMemo, useState } from 'react';
+import { BarberData } from '../data/barberData';
 import Pricing from './Pricing/Pricing';
 //import { signOut } from 'next-auth/react'
 
@@ -13,29 +12,33 @@ type TechnologyCardProps = {
   setBarberName: Dispatch<SetStateAction<string>>;
 };
 
-const TechnologyCard = ({ name, description, setBarberName}: TechnologyCardProps) => {
+const TechnologyCard = ({ name, description, setBarberName }: TechnologyCardProps) => {
   return (
-      <section 
-        className={`
+    <section
+      className={`
           button-transition hover:cursor-pointer flex flex-col justify-center p-10 duration-500 
           border-2 border-gray-300 rounded shadow-xl motion-safe:hover:scale-110
           bg-gradient-to-b from-white to-gray-300`
-        }
-        onClick={() => setBarberName(name)}
-      >
-        <h2 className={`text-2xl text-black`}>{name}</h2>
-        <p className="text-md text-gray-600">{description}</p>
-      </section>
+      }
+      onClick={() => setBarberName(name)}
+    >
+      <h2 className={`text-2xl text-black`}>{name}</h2>
+      <p className="text-md text-gray-600">{description}</p>
+    </section>
   );
 };
 
-function HomePage({barbers}: {barbers: BarberData[]}) {
+function HomePage({ barbers }: { barbers: BarberData[] }) {
   const [barberName, setBarberName] = useState<string>('');
-  const clickedBarber = barbers.filter((barber: BarberData) => barber.name === barberName)[0]
+  const clickedBarber = useMemo(() =>
+    barbers.filter((barber: BarberData) => barber.name === barberName)[0],
+    [barberName]);
 
-  return(
+  console.log(barbers)
+
+  return (
     <>
-      <div 
+      <div
         className="bg-wrap">
         <Image
           src="/barbershop.jpg"
@@ -44,12 +47,12 @@ function HomePage({barbers}: {barbers: BarberData[]}) {
         ></Image>
       </div>
 
-        {/* <button onClick={() => signOut()}>Sign Out</button> */}
-        <div className="anchor" id="meet-the-barbers"></div> 
+      {/* <button onClick={() => signOut()}>Sign Out</button> */}
+      <div className="anchor" id="meet-the-barbers"></div>
       <div
         className="pb-8 flex flex-col items-center bg-orange-200 border-white border-double border-t-4 bg-gray-700"
       >
-        <div  className="text-3xl text-white pt-8">
+        <div className="text-3xl text-white pt-8">
           <span >Meet the Barbers</span>
         </div>
         <div className="grid custom-shadow justify-center gap-3 pt-3 my-3 text-center md:grid-cols-1 lg:w-2/5">
@@ -71,7 +74,7 @@ function HomePage({barbers}: {barbers: BarberData[]}) {
         </div>
       </div>
       {clickedBarber ? (
-        <Barber 
+        <Barber
           name={clickedBarber.name}
           description={clickedBarber.description}
           pictureSrc={clickedBarber.pictureSrc}
@@ -79,34 +82,34 @@ function HomePage({barbers}: {barbers: BarberData[]}) {
       ) : (
         <div className="pt-8 pb-8  w-100 flex justify-center flex-wrap justify-around bg-gray-700">
           {barbers.map((barber) => {
-           return (
-           <div>
-             <div 
-                className="rounded-xl custom-shadow hover:cursor-pointer overflow-hidden md:h-80 md:w-80 h-48 w-48 relative"
-                onClick={()=>setBarberName(barber.name)}
-              > 
-                <Image 
-                  src={barber.pictureSrc} 
-                  layout="fill" 
-                  objectFit="contain"
+            return (
+              <div key={barber.name}>
+                <div
+                  className="rounded-xl custom-shadow hover:cursor-pointer overflow-hidden md:h-80 md:w-80 h-48 w-48 relative"
+                  onClick={() => setBarberName(barber.name)}
+                >
+                  <Image
+                    src={barber.pictureSrc}
+                    layout="fill"
+                    objectFit="contain"
                   >
-                </Image>
-            </div>
-            <div className='text-white text-2xl pt-8 text-center'>{barber.name}</div>
-          </div>
-              )
+                  </Image>
+                </div>
+                <div className='text-white text-2xl pt-8 text-center'>{barber.name}</div>
+              </div>
+            )
           })}
-          </div>
-        )
+        </div>
+      )
       }
-        <Pricing />
+      <Pricing />
       <div className="w-full h-44 md:h-36 flex flex-col items-center pt-4 bg-gray-900 border-t-2 border-white">
         <a target="_blank" rel="noopener noreferrer" href="https://www.thecut.co/" className='relative overflow-hidden rounded-lg h-20 w-40 border-4 border-white'>
-        <Image
-          src={cutLogo}
-          layout='fill'
-          objectFit='cover'
-        ></Image>
+          <Image
+            src={cutLogo}
+            layout='fill'
+            objectFit='cover'
+          ></Image>
         </a>
         <div className="text-2xl text-center text-gray-100">
           Download theCut app to schedule an appointment
--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -1,57 +1,57 @@
 import Image from "next/image"
 import { ReactElement, useState } from "react"
-import {PriceCard} from "./PriceCard"
+import { PriceCard } from "./PriceCard"
 import { dansPrices } from "../../data/priceData"
 
 
 export default function Pricing(): ReactElement {
-  const pricingBarbers = ["Gio","Dan", "Sean"]
+  const pricingBarbers = ["Gio", "Dan", "Sean"]
   const [selected, setSelected] = useState("");
   return (
     <div className="w-100 pb-16 bg-gray-700 flex-col items-center">
       <div className="w-100 pt-16 flex-col justify-center items-center border-white border-double border-t-4 w-full">
         <div className="flex justify-center w-100">
 
-        <div className="anchor" id="pricing"></div> 
+          <div className="anchor-pricing" id="pricing"></div>
           <a className="custom-shadow mx-4 flex-col bg-gray-200 items-center rounded-lg border shadow-2xl md:flex-row md:max-w-xl hover:bg-gray-100 hover:cursor-pointer">
             <div className="relative overflow-hidden border border-slate-200 h-32 rounded-lg w-24">
-                    <Image 
-                      src={"https://picsum.photos/301/300"} 
-                      layout='fill'
-                      objectFit="cover"
-                      >
-                    </Image>
+              <Image
+                src={"https://picsum.photos/301/300"}
+                layout='fill'
+                objectFit="cover"
+              >
+              </Image>
             </div>
             <div className="flex justify-between w-full leading-normal">
-                <h5 className="pl-2 mb-2 text-center text-xl font-bold tracking-tight text-gray-600">{pricingBarbers[0]}</h5>
+              <h5 className="pl-2 mb-2 text-center text-xl font-bold tracking-tight text-gray-600">{pricingBarbers[0]}</h5>
             </div>
           </a>
 
           <a className="custom-shadow mx-4 flex-col bg-blue-200 items-center rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 hover:cursor-pointer">
             <div className="relative overflow-hidden border border-slate-200 h-32 rounded-lg w-24">
-                    <Image 
-                      src={"https://picsum.photos/300/300"} 
-                      layout='fill'
-                      objectFit="cover"
-                      >
-                    </Image>
+              <Image
+                src={"https://picsum.photos/300/300"}
+                layout='fill'
+                objectFit="cover"
+              >
+              </Image>
             </div>
             <div className="flex justify-between w-full leading-normal">
-                <h5 className="pl-2 mb-2 text-center text-xl font-bold tracking-tight text-blue-600">{pricingBarbers[1]}</h5>
+              <h5 className="pl-2 mb-2 text-center text-xl font-bold tracking-tight text-blue-600">{pricingBarbers[1]}</h5>
             </div>
           </a>
 
           <a className="custom-shadow mx-4 flex-col bg-gray-200 items-center rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 hover:cursor-pointer">
             <div className="relative overflow-hidden border border-slate-200 h-32 rounded-lg w-24">
-                    <Image 
-                      src={"https://picsum.photos/302/300"} 
-                      layout='fill'
-                      objectFit="cover"
-                      >
-                    </Image>
+              <Image
+                src={"https://picsum.photos/302/300"}
+                layout='fill'
+                objectFit="cover"
+              >
+              </Image>
             </div>
             <div className="flex justify-between w-full leading-normal">
-                <h5 className="pl-2 mb-2 text-center text-xl font-bold tracking-tight text-gray-600">{pricingBarbers[2]}</h5>
+              <h5 className="pl-2 mb-2 text-center text-xl font-bold tracking-tight text-gray-600">{pricingBarbers[2]}</h5>
             </div>
           </a>
 
@@ -59,22 +59,22 @@ export default function Pricing(): ReactElement {
         </div>
       </div>
 
-        <div className="pt-8 text-center text-white text-2xl">Select a Barber to see available cuts</div>
+      <div className="pt-8 text-center text-white text-2xl">Select a Barber to see available cuts</div>
 
-        <div className="flex justify-center w-100">
+      <div className="flex justify-center w-100">
         <table className="border-gray-700 border-2 shadow-2xl">
           <tbody>
-      {dansPrices.priceData.map((priceCard) => (
-        <tr key={priceCard.haircut} className="w-100 bg-gray-700 flex justify-center">
-          <PriceCard 
-            haircut={priceCard.haircut}
-            description={priceCard.description} 
-            price={priceCard.price}
-          />
-        </tr>
-      ))}
-</tbody>
-      </table>
+            {dansPrices.priceData.map((priceCard) => (
+              <tr key={priceCard.haircut} className="w-100 bg-gray-700 flex justify-center">
+                <PriceCard
+                  haircut={priceCard.haircut}
+                  description={priceCard.description}
+                  price={priceCard.price}
+                />
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
     </div>
