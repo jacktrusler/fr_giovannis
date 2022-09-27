94b8abc7cbaa7f0bbe59f7a629e564cf4cf08744
Jack Trusler <jtrus93@gmail.com>
2022-09-27 16:56:09 -0400

    prices route rename

--- src/mongoDB/model/barbers.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, models, model } from 'mongoose'
-
-const pricesSchema = new Schema({
-  haircut: String,
-  description: String,
-  price: String,
-})
-
-export const Prices = models.Prices || model('prices', pricesSchema)
